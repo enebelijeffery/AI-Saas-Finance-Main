933e24208ff9512d4735d60c10377f89d51f1423
enebelijeffery <enebelijeffery@gmail.com>
2025-02-11 08:26:50 -0800

    commit

--- components/hero.jsx
+++ components/hero.jsx
@@ -42,7 +42,7 @@ const HeroSection = () => {
               Get Started
             </Button>
           </Link>
-          <Link href="https://github.com/ezeslucky/Ai-Saas-Finance.git">
+          <Link href="https://github.com/enebelijeffery/AI-Saas-Finance-Main">
             <Button size="lg" variant="outline" className="px-8">
               Github
             </Button>
